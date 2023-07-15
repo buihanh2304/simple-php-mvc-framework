83732904bb71fc5090f8e7d796605f5f35cdf4ca
Bui The Hanh <buihanh2304@gmail.com>
2023-07-15 20:28:22 +0700

    [chore]

--- system/Classes/Auth.php
+++ system/Classes/Auth.php
@@ -2,7 +2,7 @@
 
 /*
 // This file is a part of K-MVC
-// version: 1.1.0
+// version: 2.x
 // author: MrKen
 // website: https://vdevs.net
 // github: https://github.com/buihanh2304/simple-php-mvc-framework
